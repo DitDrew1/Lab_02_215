c76e94f96149958ce4864c513e00de546097d8d6
Andrew Joseph Snyder <snyde328@purdue.edu>
2023-01-19 13:58:28 -0500

    Final lab 02

--- Lab_02_215/Lab_02_215.cpp
+++ Lab_02_215/Lab_02_215.cpp
@@ -1,9 +1,11 @@
-// Lab_02_215.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lab_02_215.cpp : This program will take in two numbers inputted by the user and perform Ax+B=0, and solve for X and print it.
+// Written by Andrew Snyder
+// 1/19/2023
 
 #include <iostream>
 
 using namespace std;
+//main
 int main()
 {
 	float A;
@@ -20,14 +22,3 @@ int main()
 	cout << "\nThe Answer is:" ;
 	cout << "\nx=" << X;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
